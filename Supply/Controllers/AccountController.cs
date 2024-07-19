c11e10af128217bb6dc0c6bd2a9fcb2bb39aa1f4
christina ayad <christien.ayed@outlook.com>
2024-07-19 15:21:48 +0300

    Adding Account

---
+++ Supply/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Supply.Models;
+using Supply.Models.DTO;
+
+namespace Supply.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+
+            _userManager = userManager;
+        }
+        [HttpPost("[Action]")]
+        public async Task<IActionResult> Register(Registerdto user)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser appuser = new()
+                {
+                    Name= user.Username,
+                    UserName=user.Username,
+                    Email = user.Email,
+                    Mobile = user.Mobile,
+
+                };
+                IdentityResult result = await _userManager.CreateAsync(appuser, user.Password);
+                if (result.Succeeded)
+                {
+                    return Ok("sucess");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return BadRequest(ModelState);
+        }
+
+        // [HttpPost("[Login]")]
+        [HttpPost("[Action]")]
+        public async Task<IActionResult> Login(Logindto login)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser? user = await _userManager.FindByNameAsync(login.Username);
+                if(user!=null)
+                {
+                    if( await _userManager.CheckPasswordAsync(user,login.Password))
+                    {
+                        return Ok("Token");
+                    }
+                    else
+                    {
+                        return Unauthorized();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User Name is valid");
+                }
+              
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
